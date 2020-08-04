76912b2cd3e692361778106bb9e615b9d1f15124
Excellent-Developer <fazylalmeeve@gmail.com>
2020-08-03 20:24:19 -0400

    fix something

--- src/services/MapData.js
+++ src/services/MapData.js
@@ -1,31 +1,20 @@
-import { getName } from "country-list";
-
 const countryInfo = require("./country.json");
-const countryGeocode = require("./countrycode-to-latlng.json");
 
 const arrangeCountryInfo = () => {
   const countries = {};
   const list = [];
   countryInfo.hits.hits.map((item) => {
-    const geocode = countryGeocode[item._source.Country.toLocaleLowerCase()];
-    if (geocode) {
-      if (!countries[item._source.Country]) {
-        countries[item._source.Country] = {
-          lat: geocode[0],
-          lng: geocode[1],
-          name: getName(item._source.Country),
-          sources: [item._source],
-        };
-        list.push(item._source.Country);
-      } else {
-        countries[item._source.Country].sources = arraySourceByDate(
-          countries[item._source.Country].sources,
-          item._source
-        );
-      }
+    if (!countries[item._source.Country]) {
+      countries[item._source.Country] = {
+        sources: [item._source],
+      };
+    } else {
+      countries[item._source.Country].sources = arraySourceByDate(
+        countries[item._source.Country].sources,
+        item._source
+      );
     }
   });
-  countries.list = list;
   return countries;
 };
 
